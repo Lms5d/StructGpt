242f65602d5f89ff8e7836d5b6265b08ab15a350
LuoZhhh <zihanluo@hust.edu.cn>
2025-04-20 15:24:50 +0000

    change api-keyy to api-key

--- structgpt_for_webqsp.py
+++ structgpt_for_webqsp.py
@@ -1189,7 +1189,7 @@ def parse_args():
     parser.add_argument('--device', default=0, help='the gpu device')
     parser.add_argument('--topk', default=10, type=int, help='retrieve the topk score paths')
     parser.add_argument('--max_tokens', default=10, type=int, help='retrieve the topk score paths')
-    parser.add_argument('--api_keyy', default="xxxx", type=str)
+    parser.add_argument('--api_key', default="xxxx", type=str)
     parser.add_argument('--filter_score', default=0.0, type=float, help='the minimal cosine similarity')
     parser.add_argument('--kg_source_path', default=None, help='the sparse triples file')
     parser.add_argument('--ent_type_path', default=None, help='the file of entities type of sparse triples')
